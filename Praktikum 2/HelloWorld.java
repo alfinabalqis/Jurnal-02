c816c6b639f2a4643f24088eae9e0d04ebeab770
alfinabalqis <alfinabalqis28@gmail.com>
2021-09-27 08:03:42 +0700

    first commit

---
+++ Praktikum 2/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Nama: Alfina!");
+		System.out.println("NIM : D3IF 6706213133");
+		System.out.println("Kelas : 45-GAB1");
+	}
+}
